80c9901b8ac5a3724249938fa3c84d9bea66f8dc
sahin <osumatu@gmail.com>
2021-11-16 20:08:39 +0100

    Fixed the order of deployments

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
+kubectl apply -f local-pvc.yaml
 kubectl apply -f mssql-plat-depl.yaml
 kubectl apply -f users-depl.yaml
 kubectl apply -f events-depl.yaml
-kubectl apply -f local-pvc.yaml
 kubectl apply -f hangbuddy-np-srv.yaml
